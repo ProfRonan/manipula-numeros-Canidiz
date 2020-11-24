4835c07931e67da753c08c965e35058113563b9f
Canidiz <pmiguelneto@hotmail.com>
2020-11-24 14:46:17 +0000

    Atividade completa

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
-
+    A = int(input("Digite um número inteiro\n"))
+    B = int(input("Digite um número inteiro\n"))
+    C = float(input("Digite um número real\n"))
+    A1 = 2 * A + 0.5 * B
+    B1 = 3 * A + C 
+    C1 = C ** 3 
+    print(f"Valor da opção 1: {A1}.\nValor da opção 2: {B1}.\nValor da opção 3: {C1}.")
 
 if __name__ == '__main__':
     main()
